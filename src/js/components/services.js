ae7ea590b59ffe2c0337df5a4247bee276a54023
Vitalie Rosescu <vitalier@hotmail.be>
2024-11-06 13:35:32 +0100

    fixes

--- src/js/components/services.js
+++ src/js/components/services.js
@@ -1,34 +1,34 @@
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger)
 
 export default function services() {
-  let mm = gsap.matchMedia();
+  let mm = gsap.matchMedia()
 
-  mm.add('(min-width: 991px)', (context) => {
-    gsap.defaults({ ease: 'none' });
+  mm.add('(min-width: 991px)', context => {
+    gsap.defaults({ ease: 'none' })
 
-    const services = document.querySelectorAll('.services_content-item');
-    const tabs = document.querySelectorAll('.overview_tab-button');
+    const services = document.querySelectorAll('.services_content-item')
+    const tabs = document.querySelectorAll('.overview_tab-button')
 
-    const image = document.querySelector('.services_image');
-    const navbar = document.querySelector('.navbar_wrapper');
+    const image = document.querySelector('.services_image')
+    const navbar = document.querySelector('.navbar_wrapper')
 
-    const sectionComponent = document.querySelector('.services_component');
-    const sectionTrigger1 = document.querySelector('.services_trigger._1');
-    const sectionTrigger2 = document.querySelector('.services_trigger._2');
-    const sectionTrigger3 = document.querySelector('.services_trigger._3');
+    const sectionComponent = document.querySelector('.services_component')
+    const sectionTrigger1 = document.querySelector('.services_trigger._1')
+    const sectionTrigger2 = document.querySelector('.services_trigger._2')
+    const sectionTrigger3 = document.querySelector('.services_trigger._3')
 
     const enterAnimation = (service, tab) => {
-      service.classList.add('is-active');
-      tab.classList.add('is-active');
-    };
+      service.classList.add('is-active')
+      tab.classList.add('is-active')
+    }
 
     const leaveAnimation = (service, tab) => {
-      service.classList.remove('is-active');
-      tab.classList.remove('is-active');
-    };
+      service.classList.remove('is-active')
+      tab.classList.remove('is-active')
+    }
 
     // TIMELINE 1
     gsap.timeline({
@@ -38,13 +38,13 @@ export default function services() {
         end: 'bottom bottom',
         onToggle: ({ self, isActive }) => {
           if (isActive) {
-            enterAnimation(services[0], tabs[0]);
+            enterAnimation(services[0], tabs[0])
           } else {
-            leaveAnimation(services[0], tabs[0]);
+            leaveAnimation(services[0], tabs[0])
           }
         },
       },
-    });
+    })
 
     // TIMELINE 2
     gsap.timeline({
@@ -55,14 +55,14 @@ export default function services() {
         onToggle: ({ self, isActive }) => {
           if (isActive) {
             setTimeout(() => {
-              enterAnimation(services[1], tabs[1]);
-            }, 100);
+              enterAnimation(services[1], tabs[1])
+            }, 20)
           } else {
-            leaveAnimation(services[1], tabs[1]);
+            leaveAnimation(services[1], tabs[1])
           }
         },
       },
-    });
+    })
 
     // TIMELINE 3
     gsap.timeline({
@@ -73,17 +73,17 @@ export default function services() {
         onToggle: ({ self, isActive }) => {
           if (isActive) {
             setTimeout(() => {
-              enterAnimation(services[2], tabs[2]);
-            }, 100);
+              enterAnimation(services[2], tabs[2])
+            }, 100)
           } else {
-            leaveAnimation(services[2], tabs[2]);
+            leaveAnimation(services[2], tabs[2])
           }
         },
         onLeave: () => {
-          enterAnimation(services[2], tabs[2]);
+          enterAnimation(services[2], tabs[2])
         },
       },
-    });
+    })
 
     // hide NAV
     let navTl = gsap.timeline({
@@ -93,18 +93,18 @@ export default function services() {
         end: 'bottom 50%',
         onToggle: ({ self, isActive }) => {
           if (isActive) {
-            console.log('section is active');
-            navTl.play;
+            console.log('section is active')
+            navTl.play
           } else {
-            console.log('section is NOT active');
-            navTl.reverse();
+            console.log('section is NOT active')
+            navTl.reverse()
           }
         },
         onEnter: () => {
-          navTl.play();
+          navTl.play()
         },
       },
-    });
+    })
 
     navTl
       .to(navbar, {
@@ -114,7 +114,7 @@ export default function services() {
       .to(navbar, {
         duration: 0,
         pointerEvents: 'none',
-      });
+      })
 
     // image
     gsap
@@ -134,13 +134,13 @@ export default function services() {
           ease: 'Quart.easeInOut',
           y: '20%',
           rotateZ: '0',
-        }
+        },
       )
       .to(image, {
         scale: 2.5,
         ease: 'Quart.easeInOut',
         y: '30%',
-      });
+      })
 
     // blob
     gsap
@@ -158,8 +158,8 @@ export default function services() {
         {
           y: '40%',
           scale: 1,
-        }
-      );
+        },
+      )
 
     // scrollbar
     gsap
@@ -173,14 +173,14 @@ export default function services() {
       })
       .to('.services_scroll-progress', {
         width: '100%',
-      });
+      })
 
     /**
      * Remove window resize eventlistener
      */
     return () => {
-      window.removeEventListener('resize', context.onResize);
-      document.querySelector('.services_trigger').style.height = '100svh';
-    };
-  });
+      window.removeEventListener('resize', context.onResize)
+      document.querySelector('.services_trigger').style.height = '100svh'
+    }
+  })
 }
